0779916a1b77c6041ba276546778531a2a0e021a
Divya Motghare <divyamotghare9@gmail.com>
2023-10-03 14:35:24 +0530

    added the rupee symbol

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -13,12 +13,12 @@ export default function Friend({
 
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.name} {Math.abs(friend.balance)}
+          You owe {friend.name} &#8377;{Math.abs(friend.balance)}
         </p>
       )}
       {friend.balance > 0 && (
         <p className="green">
-          {friend.name} owes you {Math.abs(friend.balance)}
+          {friend.name} owes you &#8377;{Math.abs(friend.balance)}
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
